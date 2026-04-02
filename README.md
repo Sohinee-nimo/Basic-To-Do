0433ee2a4fb48cbaf4737ec8a3faca5852a42d88
Sohinee Mondal <sohineemondal@example.com>
2026-04-02 10:35:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Untitled
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/editor/sohinee-mondal/pen/019d4c80-d9d4-7f60-a884-a1d957c22c30](https://codepen.io/editor/sohinee-mondal/pen/019d4c80-d9d4-7f60-a884-a1d957c22c30).
+
---
+++ dist/script.js
@@ -0,0 +1,22 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let taskText = input.value;
+
+  if (taskText === "") return;
+
+  let li = document.createElement("li");
+  li.textContent = taskText;
+
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(deleteBtn);
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
